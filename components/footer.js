5dbddf8f19a309e530b8025de1c92c6e5e19f14b
NinaAurora <ninika@gmail.com>
2022-03-15 14:11:11 +0100

    started working on footer, added feather icons

--- components/footer.js
+++ components/footer.js
@@ -1,6 +1,50 @@
-function Footer() {
-    return <div>copyright blabla</div>
-  }
-  
-  export default Footer
-  
+import Image from 'next/image'
+
+import { Facebook } from 'react-feather';
+import { Instagram } from 'react-feather';
+import { Linkedin } from 'react-feather';
+import { Eye } from 'react-feather';
+
+
+export default function Footer() {
+  return (
+    <div className="bg-blue-600">
+
+      <div className="container mx-auto px-8 md:px-0 py-8 text-white">
+
+
+        <div className="flex justify-center items-center">
+          Like what you <Eye color="red" size={48} className="m-2" />?
+        </div>
+
+
+        <div className="grid grid-cols-3 justify-items-center">
+          <div>
+            © Anita Ponne
+          </div>
+
+
+          <div className="flex gap-4">
+
+          <Facebook color="red" size={48} />
+          <Instagram color="red" size={48} />
+          <Linkedin color="red" size={48} />
+
+           
+          </div>
+
+          <div>
+            Contact
+          </div>
+        </div>
+
+        <div className="text-center">
+          Website by Nina Aurora
+        </div>
+      </div>
+
+    </div>
+
+  )
+
+}

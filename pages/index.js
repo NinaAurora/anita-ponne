ac8038c227c95d9d3afe3e793c8cff7a65849c27
NinaAurora <ninika@gmail.com>
2022-03-31 15:26:00 +0200

    Changed placeholder image and added hover effect

--- pages/index.js
+++ pages/index.js
@@ -1,39 +1,38 @@
-import Head from 'next/head'
+import Link from 'next/link'
 import Image from 'next/image'
-import styles from '../styles/Home.module.css'
-
-import { Eye } from 'react-feather';
-
-import brushes from '../public/brushes.jpg'
-import hands from '../public/hands.jpg'
-import palette from '../public/palette.jpg'
+import { ArrowRight } from 'react-feather';
+import abstract from '../public/abstract.jpg'
 
 export default function HomePage() {
   return (
 
-  <div className="container mx-auto px-8 md:px-0">
+    <div className="container mx-auto md:px-0 overflow-hidden">
+      <div className="relative h-screen">
+        <Image
+          src={abstract}
+          alt="Hands covered in paint"
+          layout="fill"
+          objectFit="cover"
+          className="opacity-75"
+          />
 
-    <div className="relative h-screen">
-      <Image
-        src={hands}
-        alt="Hands covered in paint"
-        layout="fill"
-        objectFit="cover"
-        className="opacity-75"
-        />
-    
-      <div className="drop-shadow-lg absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 text-white text-center w-full">
-        <h1 className="uppercase text-6xl font-medium">
-          Geometrisch abstract
-        </h1>
-        <p className="text-xl font-medium">
-          Met invloeden van Sesamstraat en jaren 60 kunst.
-        </p>
+        <Link href="/gallery">
+          <a>
+            <div className="drop-shadow-lg absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 text-white text-center w-full px-8 transition ease-in-out duration-200 hover:skew-y-3 hover:scale-150">
+              <h1 className="uppercase text-4xl md:text-6xl font-medium">
+                Geometrisch abstract
+              </h1>
+              <div className="flex items-center justify-center">
+                <p className="pt-4 md:pt-0 text-xl font-medium px-4 md:px-0">
+                  Met invloeden van Sesamstraat en jaren 60 kunst 
+                </p>
+                <ArrowRight color="white" size={32} className="pl-2 " />
+              </div>
+            </div>
+          </a>
+        </Link>
       </div>
     </div>
-  
-  </div>
-
   )
 }
  

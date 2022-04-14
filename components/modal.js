17d275394996a6b8149616a2285d615c2e0880ae
NinaAurora <ninika@gmail.com>
2022-04-14 13:17:19 +0200

    Styling for the gallery pop-up buttons

--- components/modal.js
+++ components/modal.js
@@ -22,19 +22,19 @@ const modal = ({
         >
           <span
             onClick={handleClick}
-            className="z-20 dismiss text-4xl text-white absolute top-0 right-0 m-8 cursor-pointer"
+            className="w-14 h-14 text-center z-20 dismiss text-4xl text-white absolute top-0 right-0 m-8 cursor-pointer p-2 m-4 cursor-pointer rounded-full transition ease-in-out bg-slate-900/30 hover:bg-slate-900/10"
           >
             X
           </span>
           <div
-            className="flex overlay  items-center absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2"
+            className="flex overlay items-center absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2"
             onClick={handleClick}
           >
-            <div onClick={handelRotationLeft} className="p-8 cursor-pointer">
-              <div>
+            <button onClick={handelRotationLeft} className="p-2 m-4 cursor-pointer rounded-full transition ease-in-out bg-slate-900/30 hover:bg-slate-900/10">
+              <div className="   ">
                 <ArrowLeft size={50} color="white" />
               </div>
-            </div>
+            </button>
 
             <div className="w-[700px] h-screen relative">
               <Image
@@ -47,7 +47,7 @@ const modal = ({
               />
             </div>
 
-            <div onClick={handelRotationRight} className="p-8 cursor-pointer">
+            <div onClick={handelRotationRight} className="p-2 m-4 cursor-pointer rounded-full transition ease-in-out bg-slate-900/30 hover:bg-slate-900/10">
               <div>
                 <ArrowRight size={50} color="white" />
               </div>
